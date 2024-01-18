427f9e2ccc00fcc206dec7138c4d0783678cc6b9
Zhanuzak <anashov17@gmail.com>
2024-01-18 21:02:29 +0600

    commit create test

--- src/main/java/com/example/aiyl_bank/dto/ClubRequest.java
+++ src/main/java/com/example/aiyl_bank/dto/ClubRequest.java
@@ -1,12 +1,19 @@
 package com.example.aiyl_bank.dto;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import java.math.BigDecimal;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class ClubRequest {
     private String name;
     private BigDecimal price;
+
+    public ClubRequest(String name, BigDecimal price) {
+        this.name = name;
+        this.price = price;
+    }
 }
